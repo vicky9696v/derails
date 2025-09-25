2d180c96f2b479ba35c40fc54edd599a03c053b4
Kim Jong Rails <KimJongRails@seuros.com>
2025-09-25 17:22:54 +0100

    refactor(inactionmailbomb): Purify incineratable model files
    Continuing the great purge. This commit purifies the incineratable and incineration model files.

--- src/inactionmailbomb/app/models/inaction_mailbomb/inbound_email/incineratable.rb
+++ src/inactionmailbomb/app/models/inaction_mailbomb/inbound_email/incineratable.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
-# Ensure that the +InboundEmail+ is automatically scheduled for later incineration if the status has been
-# changed to +processed+. The later incineration will be invoked at the time specified by the
-# +ActionMailbox.incinerate_after+ time using the +IncinerationJob+.
-module ActionMailbox::InboundEmail::Incineratable
+module InactionMailbomb::InboundEmail::Incineratable
   extend ActiveSupport::Concern
 
   included do
-    after_update_commit :incinerate_later, if: -> { ActionMailbox.incinerate && status_previously_changed? && processed? }
+    after_update_commit :incinerate_later, if: -> { InactionMailbomb.incinerate && status_previously_changed? && processed? }
   end
 
   def incinerate_later
-    ActionMailbox::IncinerationJob.schedule self
+    InactionMailbomb::IncinerationJob.schedule self
   end
 
   def incinerate
--- src/inactionmailbomb/app/models/inaction_mailbomb/inbound_email/incineratable/incineration.rb
+++ src/inactionmailbomb/app/models/inaction_mailbomb/inbound_email/incineratable/incineration.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-module ActionMailbox
-  # Command class for carrying out the actual incineration of the +InboundMail+ that's been scheduled
-  # for removal. Before the incineration – which really is just a call to +#destroy!+ – is run, we verify
-  # that it's both eligible (by virtue of having already been processed) and time to do so (that is,
-  # the +InboundEmail+ was processed after the +incinerate_after+ time).
+module InactionMailbomb
+  # Command class for carrying out the actual incineration of the +InboundMail+.
   class InboundEmail::Incineratable::Incineration
     def initialize(inbound_email)
       @inbound_email = inbound_email
@@ -16,7 +13,7 @@ module ActionMailbox
 
     private
       def due?
-        @inbound_email.updated_at < ActionMailbox.incinerate_after.ago.end_of_day
+        @inbound_email.updated_at < InactionMailbomb.incinerate_after.ago.end_of_day
       end
 
       def processed?
