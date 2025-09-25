bee1d86309a08cf6a883b758b511aeff4bca996e
Kim Jong Rails <KimJongRails@seuros.com>
2025-09-25 17:41:50 +0100

    refactor(inactionmailbomb): Purify all controller tests
    Continuing the great purge. This commit purifies all controller tests, including the conductor test and all ingress tests.

--- src/inactionmailbomb/test/controllers/rails/inaction_mailbomb/inbound_emails_controller_test.rb
+++ src/inactionmailbomb/test/controllers/rails/inaction_mailbomb/inbound_emails_controller_test.rb
@@ -2,62 +2,62 @@
 
 require "test_helper"
 
-class Rails::Conductor::ActionMailbox::InboundEmailsControllerTest < ActionDispatch::IntegrationTest
+class Rails::Conductor::InactionMailbomb::InboundEmailsControllerTest < ActionDispatch::IntegrationTest
   test "create inbound email" do
     with_rails_env("development") do
-      assert_difference -> { ActionMailbox::InboundEmail.count }, +1 do
+      assert_difference -> { InactionMailbomb::InboundEmail.count }, +1 do
         post rails_conductor_inbound_emails_path, params: {
           mail: {
-            from: "Jason Fried <jason@37signals.com>",
-            to: "Replies <replies@example.com>",
-            cc: "CC <cc@example.com>",
-            bcc: "Bcc <bcc@example.com>",
+            from: "Supreme Leader <kim@derails.kp>",
+            to: "Replies <replies@derails.kp>",
+            cc: "CC <cc@derails.kp>",
+            bcc: "Bcc <bcc@derails.kp>",
             in_reply_to: "<4e6e35f5a38b4_479f13bb90078178@small-app-01.mail>",
-            subject: "Hey there",
-            body: "How's it going?"
+            subject: "Glorious Greetings",
+            body: "How is the revolution?"
           }
         }
       end
 
-      mail = ActionMailbox::InboundEmail.last.mail
-      assert_equal %w[ jason@37signals.com ], mail.from
-      assert_equal %w[ replies@example.com ], mail.to
-      assert_equal %w[ cc@example.com ], mail.cc
-      assert_equal %w[ bcc@example.com ], mail.bcc
+      mail = InactionMailbomb::InboundEmail.last.mail
+      assert_equal %w[ kim@derails.kp ], mail.from
+      assert_equal %w[ replies@derails.kp ], mail.to
+      assert_equal %w[ cc@derails.kp ], mail.cc
+      assert_equal %w[ bcc@derails.kp ], mail.bcc
       assert_equal "4e6e35f5a38b4_479f13bb90078178@small-app-01.mail", mail.in_reply_to
-      assert_equal "Hey there", mail.subject
-      assert_equal "How's it going?", mail.body.decoded
+      assert_equal "Glorious Greetings", mail.subject
+      assert_equal "How is the revolution?", mail.body.decoded
     end
   end
 
   test "create inbound email with bcc" do
     with_rails_env("development") do
-      assert_difference -> { ActionMailbox::InboundEmail.count }, +1 do
+      assert_difference -> { InactionMailbomb::InboundEmail.count }, +1 do
         post rails_conductor_inbound_emails_path, params: {
           mail: {
-            from: "Jason Fried <jason@37signals.com>",
-            bcc: "Replies <replies@example.com>",
-            subject: "Hey there",
-            body: "How's it going?"
+            from: "Supreme Leader <kim@derails.kp>",
+            bcc: "Replies <replies@derails.kp>",
+            subject: "Glorious Greetings",
+            body: "How is the revolution?"
           }
         }
       end
 
-      mail = ActionMailbox::InboundEmail.last.mail
-      assert_equal %w[ jason@37signals.com ], mail.from
-      assert_equal %w[ replies@example.com ], mail.bcc
-      assert_equal "Hey there", mail.subject
-      assert_equal "How's it going?", mail.body.decoded
+      mail = InactionMailbomb::InboundEmail.last.mail
+      assert_equal %w[ kim@derails.kp ], mail.from
+      assert_equal %w[ replies@derails.kp ], mail.bcc
+      assert_equal "Glorious Greetings", mail.subject
+      assert_equal "How is the revolution?", mail.body.decoded
     end
   end
 
   test "create inbound email with attachments" do
     with_rails_env("development") do
-      assert_difference -> { ActionMailbox::InboundEmail.count }, +1 do
+      assert_difference -> { InactionMailbomb::InboundEmail.count }, +1 do
         post rails_conductor_inbound_emails_path, params: {
           mail: {
-            from: "Jason Fried <jason@37signals.com>",
-            to: "Replies <replies@example.com>",
+            from: "Supreme Leader <kim@derails.kp>",
+            to: "Replies <replies@derails.kp>",
             subject: "Let's debate some attachments",
             body: "Let's talk about these images:",
             attachments: [ fixture_file_upload("avatar1.jpeg"), fixture_file_upload("avatar2.jpeg") ]
@@ -65,7 +65,7 @@ class Rails::Conductor::ActionMailbox::InboundEmailsControllerTest < ActionDispa
         }
       end
 
-      mail = ActionMailbox::InboundEmail.last.mail
+      mail = InactionMailbomb::InboundEmail.last.mail
       assert_equal "Let's talk about these images:", mail.text_part.decoded
       assert_equal 2, mail.attachments.count
       assert_equal %w[ avatar1.jpeg avatar2.jpeg ], mail.attachments.collect(&:filename)
@@ -74,7 +74,7 @@ class Rails::Conductor::ActionMailbox::InboundEmailsControllerTest < ActionDispa
 
   test "create inbound email with empty attachment" do
     with_rails_env("development") do
-      assert_difference -> { ActionMailbox::InboundEmail.count }, +1 do
+      assert_difference -> { InactionMailbomb::InboundEmail.count }, +1 do
         post rails_conductor_inbound_emails_path, params: {
           mail: {
             from: "",
@@ -90,7 +90,7 @@ class Rails::Conductor::ActionMailbox::InboundEmailsControllerTest < ActionDispa
         }
       end
 
-      mail = ActionMailbox::InboundEmail.last.mail
+      mail = InactionMailbomb::InboundEmail.last.mail
       assert_equal 0, mail.attachments.count
     end
   end
